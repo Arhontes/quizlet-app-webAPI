365c884cb1d546afb5d4f9192ea879631c348098
Alex Betke <arhontes@yandex.ru>
2022-09-16 10:22:02 +0300

    create: AddWordsModuleRequest

---
+++ quizlet-app-webAPI/Models/AddWordsModuleRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace quizlet_app_webAPI.Models
+{
+    public class AddWordsModuleRequest
+    {
+        public string Name { get; set; }
+        public string Words { get; set; }
+        public DateTime createDate { get; set; }
+    }
+}
