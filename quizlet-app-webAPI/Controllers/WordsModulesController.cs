3b44b926e3ea8d86d7c4ee255e7f5bd8fc586364
Alex Betke <arhontes@yandex.ru>
2022-09-16 10:09:58 +0300

    create: WordsModules controller

---
+++ quizlet-app-webAPI/Controllers/WordsModulesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using quizlet_app_webAPI.Data;
+
+namespace quizlet_app_webAPI.Controllers
+{   [ApiController]
+    [Route("api/wordsmodules")]
+    public class WordsModulesController : Controller
+    {
+        private readonly WordsModuleAPIDbContext dbContext;
+
+        public WordsModulesController(WordsModuleAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetWordsModules()
+        {
+            return Ok(dbContext.WordsModules.ToList());
+            
+        }
+    }
+}
