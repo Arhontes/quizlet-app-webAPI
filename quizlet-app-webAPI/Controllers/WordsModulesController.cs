ca13aa70fc2c52fa0bae8cdadebcc93220b70103
Alex Betke <arhontes@yandex.ru>
2022-09-16 10:22:41 +0300

    add: post request method

--- quizlet-app-webAPI/Controllers/WordsModulesController.cs
+++ quizlet-app-webAPI/Controllers/WordsModulesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using quizlet_app_webAPI.Data;
+using quizlet_app_webAPI.Models;
 
 namespace quizlet_app_webAPI.Controllers
 {   [ApiController]
@@ -14,9 +16,25 @@ namespace quizlet_app_webAPI.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetWordsModules()
+        public async Task<IActionResult> GetWordsModules()
         {
-            return Ok(dbContext.WordsModules.ToList());
+            return Ok(await dbContext.WordsModules.ToListAsync());
+            
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddWordsModule(AddWordsModuleRequest addWordsModuleRequest)
+        {
+            var wordsModule = new WordsModule()
+            {
+                Id = Guid.NewGuid(),
+                Words = addWordsModuleRequest.Words,
+                CreateDate = DateTime.Now,
+                Name = addWordsModuleRequest.Name,
+            };
+            await dbContext.WordsModules.AddAsync(wordsModule);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(wordsModule);
             
         }
     }
