395dffdf83393cb7b6f60015e2eac4d55617ff36
Alex Betke <arhontes@yandex.ru>
2022-09-16 10:09:25 +0300

    edit: string[] to string

--- quizlet-app-webAPI/Models/WordsModule.cs
+++ quizlet-app-webAPI/Models/WordsModule.cs
@@ -4,7 +4,7 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
-        public string[] Words { get; set; }
+        public string Words { get; set; }
         public DateTime createDate { get; set; }
     }
 }
