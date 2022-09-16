06a0c29ad4bc16a5441c4d04e4122b74103bba70
Alex Betke <arhontes@yandex.ru>
2022-09-16 09:51:07 +0300

    create: class WordsModulesAPIDbContext

---
+++ quizlet-app-webAPI/Data/WordsModuleAPIDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using quizlet_app_webAPI.Models;
+
+namespace quizlet_app_webAPI.Data
+{
+    public class WordsModuleAPIDbContext : DbContext
+    {
+        public WordsModuleAPIDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<WordsModule> WordsModules { get; set; }
+    }
+}
