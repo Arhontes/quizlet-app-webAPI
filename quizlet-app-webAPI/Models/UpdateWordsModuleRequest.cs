ee413d8e15488e56d434b1c6e4ea717bfc2db30b
Alex Betke <arhontes@yandex.ru>
2022-09-16 10:48:36 +0300

    create: new request class

---
+++ quizlet-app-webAPI/Models/UpdateWordsModuleRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace quizlet_app_webAPI.Models
+{
+    public class UpdateWordsModuleRequest
+    {
+        public string Name { get; set; }
+        public string Words { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
