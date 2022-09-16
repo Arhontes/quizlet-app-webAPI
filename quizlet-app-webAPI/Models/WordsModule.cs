91989da0eb0111536ba8dd3dda6e388d7864b34c
Alex Betke <arhontes@yandex.ru>
2022-09-16 09:33:38 +0300

    create: class WordsModule

---
+++ quizlet-app-webAPI/Models/WordsModule.cs
@@ -0,0 +1,10 @@
+﻿namespace quizlet_app_webAPI.Models
+{
+    public class WordsModule
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string[] Words { get; set; }
+        public DateTime createDate { get; set; }
+    }
+}
