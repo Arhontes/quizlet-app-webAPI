7944bd9534ad1db1f6f05136cbcd45f0ace8ff71
Alex Betke <arhontes@yandex.ru>
2022-10-04 23:49:09 +0300

    feature(User,UserDto): create new classes with user data

---
+++ quizlet-app-webAPI/User.cs
@@ -0,0 +1,11 @@
+﻿namespace quizlet_app_webAPI
+{
+    public class User
+    {
+        public string UserName { get; set; } = string.Empty;
+
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+    }
+}
---
+++ quizlet-app-webAPI/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace quizlet_app_webAPI
+{
+    public class UserDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
