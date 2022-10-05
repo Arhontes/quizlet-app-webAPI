f23d76c6e7524564ac5cfb26f51b658e9a8ef3f9
Alex Betke <arhontes@yandex.ru>
2022-10-05 20:51:19 +0300

    feature(ApplicationUser) : create new model

---
+++ quizlet-app-webAPI/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿namespace quizlet_app_webAPI.Models
+{
+    public class ApplicationUser
+    {
+        public Guid Id { get; set; }
+      
+        public string Email { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
