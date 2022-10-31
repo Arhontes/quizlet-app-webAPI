b2f39e9ed3d1c144111ab849923e3408adf83a1f
Alex Betke <arhontes@yandex.ru>
2022-10-31 19:35:08 +0300

    refactor(AuthController) - change register data

--- quizlet-app-webAPI/Controllers/AuthController.cs
+++ quizlet-app-webAPI/Controllers/AuthController.cs
@@ -14,9 +14,10 @@ namespace quizlet_app_webAPI.Controllers
     public class AuthController : ControllerBase
     {
 
-        public static ApplicationUser newUser = new ApplicationUser();
+        public static ApplicationUser currentUser = new ApplicationUser();
         private readonly IConfiguration configuration;
         private readonly WordsModuleAPIDbContext _dbContext;
+
         public AuthController(IConfiguration configuration, WordsModuleAPIDbContext dbContext,IUserService userService)
         {
             this.configuration = configuration;
@@ -26,6 +27,14 @@ namespace quizlet_app_webAPI.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserDto request)
         {
+
+            var findUser = await _dbContext.Users.FirstOrDefaultAsync(x => x.UserName == request.UserName);
+            if (findUser != null)
+            {
+                return BadRequest($"name: {request.UserName}  is already exist");
+
+            }
+            var newUser = new ApplicationUser();
             CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
             newUser.Id = Guid.NewGuid();
             newUser.UserName = request.UserName;
@@ -33,7 +42,7 @@ namespace quizlet_app_webAPI.Controllers
             newUser.PasswordHash = passwordHash;
             newUser.PasswordSalt = passwordSalt;
 
-            await _dbContext.AddAsync(newUser);
+            await _dbContext.Users.AddAsync(newUser);
             await _dbContext.SaveChangesAsync();
 
             return Ok(newUser);
@@ -41,20 +50,73 @@ namespace quizlet_app_webAPI.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserDto request)
         {
-            var user = await _dbContext.Users.SingleOrDefaultAsync(x => x.UserName == request.UserName);
+            var user = await _dbContext.Users.FirstOrDefaultAsync(x => x.UserName == request.UserName);
+            if (currentUser.RefreshToken.Equals(Request.Cookies["refreshToken"]))
+            {
+                return Ok("You are already logged in");
+            }
             if (user == null)
             {
                 return NotFound();
             }
             if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
             {
-                return BadRequest("inccorect password");
+                return BadRequest("inccorect user name or password");
             }
 
-            string token = CreateToken(newUser);
+            string token = CreateToken(user);
+            var refreshToken = GenerateRefreshToken();
+            SetRefreshToken(refreshToken,user);
+            currentUser = user;
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(token);
+        }
+        [HttpPost("refresh-token")]
+        public async Task<ActionResult<string>> RefreshToken()
+        {
+            var refreshToken = Request.Cookies["refreshToken"];
+            
+            if (!currentUser.RefreshToken.Equals(refreshToken))
+            {
+                return Unauthorized("Invalid Refresh Token.");
+            }
+            else if (currentUser.TokenExpires < DateTime.Now)
+            {
+                return Unauthorized("Token expired.");
+            }
+
+            string token = CreateToken(currentUser);
+            var newRefreshToken = GenerateRefreshToken();
+            SetRefreshToken(newRefreshToken, currentUser);
+            await _dbContext.SaveChangesAsync();
             return Ok(token);
         }
 
+        private RefreshToken GenerateRefreshToken()
+        {
+            var refreshToken = new RefreshToken()
+            {
+                Token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64)),
+                Expires = DateTime.Now.AddDays(7),
+                Created = DateTime.Now,
+            };
+            return refreshToken;
+        }
+
+        private void SetRefreshToken(RefreshToken newRefreshToken, ApplicationUser user)
+        {
+            var cookieOptions = new CookieOptions
+            {
+                HttpOnly = true,
+                Expires = newRefreshToken.Expires,
+            };
+            Response.Cookies.Append("refreshToken", newRefreshToken.Token, cookieOptions);
+            user.RefreshToken = newRefreshToken.Token;
+            user.TokenCreated = newRefreshToken.Created;
+            user.TokenExpires = newRefreshToken.Expires;
+
+        }
         private string CreateToken(ApplicationUser user)
         {
             List<Claim> claims = new List<Claim>
