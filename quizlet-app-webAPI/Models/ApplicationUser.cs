aafd8bf7b350979e70627b71aefe3a5dcc4c01c2
Alex Betke <arhontes@yandex.ru>
2022-10-31 19:36:36 +0300

    refactor(ApllicationUser) - add new fields for refresh token]

--- quizlet-app-webAPI/Models/ApplicationUser.cs
+++ quizlet-app-webAPI/Models/ApplicationUser.cs
@@ -3,10 +3,14 @@
     public class ApplicationUser
     {
         public Guid Id { get; set; }
-      
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
         public string UserName { get; set; } = string.Empty;
+
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime TokenCreated { get; set; }
+        public DateTime TokenExpires { get; set; }
     }
 }
