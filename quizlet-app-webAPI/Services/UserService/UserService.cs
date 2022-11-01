f6e0ac48a8b363ee25620b2e438af0b6b1d9d3fe
Alex Betke <arhontes@yandex.ru>
2022-11-01 21:56:12 +0300

    feature(UserSevice) - new method get user id

--- quizlet-app-webAPI/Services/UserService/UserService.cs
+++ quizlet-app-webAPI/Services/UserService/UserService.cs
@@ -10,12 +10,22 @@ namespace quizlet_app_webAPI.Services.UserService
         {
             this.httpContextAccessor = httpContextAccessor;
         }
+        
         public string GetUserName()
         {
             var result  = string.Empty;
             if (httpContextAccessor.HttpContext!= null)
             {
                 result = httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name);
+            }
+            return result;
+        }
+        public string GetUserId()
+        {
+            var result  = string.Empty;
+            if (httpContextAccessor.HttpContext!= null)
+            {
+                result = httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
             }
             return result;
         }
