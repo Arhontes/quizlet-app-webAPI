acaaf99d1f516f35985b8212fec5774fe29b79ae
Alex Betke <arhontes@yandex.ru>
2022-11-01 21:52:01 +0300

    feature(WordsController) - implement IUserService, do user validation

--- quizlet-app-webAPI/Controllers/WordsController.cs
+++ quizlet-app-webAPI/Controllers/WordsController.cs
@@ -9,36 +9,50 @@ namespace quizlet_app_webAPI.Controllers
     [ApiController]
     [Route("api/words")]
     [EnableCors("_myAllowSpecificOrigins")]
+    [Authorize]
     public class WordsController : Controller
     {
         private readonly WordsModuleAPIDbContext dbContext;
-
-        public WordsController(WordsModuleAPIDbContext dbContext)
+        private readonly IUserService _userService;
+        public WordsController(WordsModuleAPIDbContext dbContext, IUserService userService)
         {
+            _userService = userService;
             this.dbContext = dbContext;
         }
 
         [HttpGet]
         [Route("{id:guid}")]
-        [EnableCors("_myAllowSpecificOrigins")]
         public async Task<IActionResult> GetWords([FromRoute] Guid id)
         {
-            var wordsModule = await dbContext.WordsModules.FindAsync(id);
-            if (wordsModule != null)
+            var wordsList = await dbContext.Words.Where(x => x.WordsModuleId == id).ToListAsync();
+            if(wordsList.Count == 0)
             {
-                var words = await dbContext.Words.Where(el => el.WordsModuleId == wordsModule.Id).ToListAsync();
-                return Ok(words);
+                return NotFound();
             }
-            else return BadRequest();
+            return Ok(wordsList);
         }
 
         [HttpPost]
         public async Task<IActionResult> AddWord(AddWordRequest addWordRequest)
         {
 
+            var userId = _userService.GetUserId();
+
             var module = await dbContext.WordsModules.FindAsync(Guid.Parse(addWordRequest.WordsModuleId));
+
             if (module != null)
             {
+                if (module.UserId.ToString() != userId)
+                {
+                    return BadRequest("access denied");
+                }
+
+                var currentWordsCount = dbContext.Words.Where(el => el.WordsModuleId == module.WordsModuleId).Count();
+
+                if (currentWordsCount >= 100)
+                {
+                    return BadRequest("module is full");
+                }
                 var word = new Word()
                 {
                     Definition = addWordRequest.Definition,
@@ -46,38 +60,48 @@ namespace quizlet_app_webAPI.Controllers
                     Transcription = addWordRequest.Transcription,
                     WordImgUrl = addWordRequest.WordImgUrl,
                     WordId = new Guid(),
-                    WordsModuleId = module.Id
+                    WordsModuleId = module.WordsModuleId,
+                    WordsModule = module,
+                    
                 };
-                var currentWordsCount = dbContext.Words.Where(el => el.WordsModuleId == module.Id).Count();
 
-                module.Words.Add(word);
-                if (currentWordsCount + 1 < 100)
-                {
-                    module.Words.Add(word);
-                    module.maxWordsCount++;
-                }
-                else
-                {
-                    return BadRequest("module is full");
-                }
+                await dbContext.Words.AddAsync(word);
 
+                module.WordsCount++;
                 await dbContext.SaveChangesAsync();
 
                 return Ok(word);
             }
-            return BadRequest();
+            return NotFound("module not found");
 
         }
+
         [HttpPut]
         public async Task<IActionResult> UpdateWord(UpdateWordRequest updateWordRequest)
         {
+            var userId = _userService.GetUserId();
+
+            
+
             var word = await dbContext.Words.FindAsync(Guid.Parse(updateWordRequest.WordId));
-            if (word != null)
+
+            if ( word != null)
             {
+
+                var module = await dbContext.WordsModules.FindAsync(word.WordsModuleId);
+                if (module == null)
+                {
+                    return NotFound("module not found");
+                }
+                else if (module.UserId.ToString() != userId)
+                {
+                    return BadRequest("access denied");
+                }
+
                 word.Transcription = !string.IsNullOrEmpty(updateWordRequest.Transcription) ? updateWordRequest.Transcription : word.Transcription;
-                word.Transcription = !string.IsNullOrEmpty(updateWordRequest.WordImgUrl) ? updateWordRequest.WordImgUrl : word.WordImgUrl;
-                word.Transcription = !string.IsNullOrEmpty(updateWordRequest.Definition) ? updateWordRequest.Definition : word.Definition;
-                word.Transcription = !string.IsNullOrEmpty(updateWordRequest.Meaning) ? updateWordRequest.Meaning : word.Meaning;
+                word.WordImgUrl = !string.IsNullOrEmpty(updateWordRequest.WordImgUrl) ? updateWordRequest.WordImgUrl : word.WordImgUrl;
+                word.Definition = !string.IsNullOrEmpty(updateWordRequest.Definition) ? updateWordRequest.Definition : word.Definition;
+                word.Meaning = !string.IsNullOrEmpty(updateWordRequest.Meaning) ? updateWordRequest.Meaning : word.Meaning;
 
                 await dbContext.SaveChangesAsync();
 
@@ -85,14 +109,29 @@ namespace quizlet_app_webAPI.Controllers
             }
             return NotFound();
         }
+
         [HttpDelete]
         [Route("{id:guid}")]
         public async Task<IActionResult> DeleteWord([FromRoute] Guid id)
         {
+
+            var userId = _userService.GetUserId();
+
             var word = await dbContext.Words.FindAsync(id);
             if (word != null)
             {
+                var module = await dbContext.WordsModules.FindAsync(word.WordsModuleId);
+                if (module == null)
+                {
+                    return NotFound("module not found");
+                }
+                else if (module.UserId.ToString() != userId)
+                {
+                    return BadRequest("access denied");
+                }
+
                 dbContext.Remove(word);
+                module.WordsCount --;
                 await dbContext.SaveChangesAsync();
 
                 return Ok(word);
